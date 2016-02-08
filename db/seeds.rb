c4c2d946f97d9be6bc17cf67874b2b621c9e94af
euglazer <eugenestapler@gmail.com>
2016-02-08 22:56:31 +1300

    modify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
 Pool.destroy_all
 
-# create a pool with five members
 pool = Pool.create(name: "Savings Pool")
-5.times { pool.members.create(name: Faker::Name.name) }
-
-# create initial deposits for the pool
-pool.members.each do |member|
-  amount = rand(1..10) * 100
-  Deposit.create(pool: pool, member: member, amount: amount)
-end
+3.times { pool.members.create(name: Faker::Name.name) }
